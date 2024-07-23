5ef3d18d081720752148b357b0c1779d94abdff2
Tawin <tawinturbo402@gmail.com>
2024-07-23 14:08:56 +0700

    เพิ่มเขียนบทความใหม่

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,34 +6,53 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    function index(){
-        $blogs=[
+    function index()
+    {
+        $blogs = [
 
             [
-                'title'=>"บทความที่ 1",
-                'content'=>'เนื้อหาบทความที่ 1',
-                'status'=>true
+                'title' => "บทความที่ 1",
+                'content' => 'เนื้อหาบทความที่ 1',
+                'status' => true
             ],
             [
-                'title'=>"บทความที่ 2",
-                'content'=>'เนื้อหาบทความที่ 2',
-                'status'=>false
+                'title' => "บทความที่ 2",
+                'content' => 'เนื้อหาบทความที่ 2',
+                'status' => false
             ],
             [
-                'title'=>"บทความที่ 3",
-                'content'=>'เนื้อหาบทความที่ 3',
-                'status'=>true
+                'title' => "บทความที่ 3",
+                'content' => 'เนื้อหาบทความที่ 3',
+                'status' => true
             ],
         ];
-        return view('blog',compact('blogs'));
+        return view('blog', compact('blogs'));
     }
 
-    function about(){
-        $name="Radompon Duangta";
-        $date="27 มิถุนายน 2567";
-        return view('about',compact('name','date'));
+    function about()
+    {
+        $name = "Radompon Duangta";
+        $date = "27 มิถุนายน 2567";
+        return view('about', compact('name', 'date'));
     }
-    function create(){
+
+    function create()
+    {
         return view('form');
+    }
+
+    function insert(Request $request)
+    {
+        $request->validate(
+            [
+                'title' => 'required|max:50',
+                'content' => 'required',
+            ],
+            [
+                'title.required'=>'กรุณาใส่ชื่อบทความ',
+                'title.max'=>'ชื่อบทความไม่ควรเกิน 50 ตัวอักษร',
+                'content.required'=>'กรุณาใส่เนื้อหาบทความ'
+            ]
+        );
     }
 }
